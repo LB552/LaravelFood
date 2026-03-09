8171b08a271de20369fc95ef09732a89ada64f81
Anton Börnfelt <antonb@MacBook-Pro-som-tillhor-Anton.local>
2026-03-09 13:27:04 +0100

    added database file laravel.sqlite

--- todo-app/database/migrations/2026_03_03_132232_create_products_table.php
+++ todo-app/database/migrations/2026_03_03_132232_create_products_table.php
@@ -4,22 +4,20 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('products', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->id(); // auto-increment id
+            $table->foreignId('category_id')->constrained()->onDelete('cascade'); // foreign key
+            $table->string('name'); // product name
+            $table->string('image'); // stores file path for image like "products/banana.jpg"
+            $table->decimal('price', 10, 2); // price with 2 decimals
+            $table->text('description')->nullable(); // optional description
+            $table->timestamps(); // optional but recommended
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('products');
